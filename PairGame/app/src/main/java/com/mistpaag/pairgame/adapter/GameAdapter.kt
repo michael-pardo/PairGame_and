ae96c11e4857fefb698bf0067d5b56c76ef09e21
michael Parod <michael.pardo@unillanos.edu.co>
2019-11-24 19:01:17 -0500

    Cover image #3

--- PairGame/app/src/main/java/com/mistpaag/pairgame/adapter/GameAdapter.kt
+++ PairGame/app/src/main/java/com/mistpaag/pairgame/adapter/GameAdapter.kt
@@ -1,19 +1,20 @@
 package com.mistpaag.pairgame.adapter
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.DecelerateInterpolator
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import coil.api.load
 import com.mistpaag.pairgame.R
 import com.mistpaag.pairgame.model.Level
+import com.mistpaag.pairgame.utils.Const
 import com.mistpaag.pairgame.utils.inflate
 import kotlinx.android.synthetic.main.item_game.view.*
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.view.animation.AccelerateDecelerateInterpolator
-import android.view.animation.DecelerateInterpolator
-import android.animation.ObjectAnimator
-import com.mistpaag.pairgame.utils.Const
 
 
 class GameAdapter(val itemClick:(Level)-> Unit): RecyclerView.Adapter<GameAdapter.ViewHolder>() {
@@ -22,10 +23,14 @@ class GameAdapter(val itemClick:(Level)-> Unit): RecyclerView.Adapter<GameAdapte
     val list = listOf("Holla","Hola","Holla","Hola","Holla","Hola","Holla","Hola","Holla","Hola","Holla","Hola")
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
+        Const.ultima = 10
         val view = parent.inflate(R.layout.item_game)
         val height = parent.getMeasuredHeight() / 3
-
         view.minimumHeight = height
+
+        val lp = view.getLayoutParams() as GridLayoutManager.LayoutParams
+        lp.height = height
+        view.setLayoutParams(lp)
         return ViewHolder(view, itemClick)
     }
 
@@ -34,7 +39,6 @@ class GameAdapter(val itemClick:(Level)-> Unit): RecyclerView.Adapter<GameAdapte
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         viewHolder = holder
         holder.bindView(list[position],position)
-//        holder.itemView.minimumHeight = 300
     }
 
 
@@ -48,10 +52,6 @@ class GameAdapter(val itemClick:(Level)-> Unit): RecyclerView.Adapter<GameAdapte
             with(level){
                 itemView.cons_image.layoutParams.height = itemView.layoutParams.height
                 itemView.img_card.layoutParams.height = itemView.cons_image.layoutParams.height
-                itemView.img_card.load(R.drawable.ic_launcher_foreground) {
-                    crossfade(true)
-                    placeholder(R.drawable.ic_launcher_background)
-                }
 
             }
             itemView.setOnClickListener {
@@ -62,10 +62,6 @@ class GameAdapter(val itemClick:(Level)-> Unit): RecyclerView.Adapter<GameAdapte
                     moverImage()
                     Const.ultima = position
                 }
-
-
-
-
             }
         }
 
@@ -77,9 +73,8 @@ class GameAdapter(val itemClick:(Level)-> Unit): RecyclerView.Adapter<GameAdapte
             oa1.addListener(object : AnimatorListenerAdapter() {
                 override fun onAnimationEnd(animation: Animator) {
                     super.onAnimationEnd(animation)
-                    itemView.img_card.load(R.drawable.icon_facebook) {
+                    itemView.img_card.load(R.drawable.cover_image) {
                         crossfade(true)
-                        placeholder(R.drawable.ic_launcher_background)
                     }
                     oa2.start()
                 }
